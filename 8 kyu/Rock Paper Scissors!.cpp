fe74374a8f084b2c26ebe960b04eab225be4b479
HoshinoIs <hoshinois.nhb@gmail.com>
2025-10-14 18:23:56 +0700

    floweringnight

---
+++ 8 kyu/Rock Paper Scissors!.cpp
@@ -0,0 +1,13 @@
+//https://www.codewars.com/kata/5672a98bdbdd995fad00000f/train/cpp
+
+#include <iostream>
+#include <string>
+
+std::string rps(const std::string& p1, const std::string& p2) {
+    if (p1 == p2)return "Draw!";
+    if ((p1 == "rock"     && p2 == "scissors") ||
+        (p1 == "scissors" && p2 == "paper")    ||
+        (p1 == "paper"    && p2 == "rock"))
+        return "Player 1 won!";
+    return "Player 2 won!";
+}
